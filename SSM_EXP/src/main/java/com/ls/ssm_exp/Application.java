987fa0eac268a965d2900f1cd4cb4d286bca186f
destinal <l17305042002@163.com>
2024-06-29 12:45:46 +0800

    关于Spring事务的rollbackFor指定异常类型的问题

---
+++ SSM_EXP/src/main/java/com/ls/ssm_exp/Application.java
@@ -0,0 +1,19 @@
+package com.ls.ssm_exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: ls
+ * @Date: 2024/6/2911:20
+ */
+
+@SpringBootApplication // 标记这是一个Spring Boot应用
+
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // 启动Spring Boot应用
+    }
+}
