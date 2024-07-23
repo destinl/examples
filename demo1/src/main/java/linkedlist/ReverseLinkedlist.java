f8d7bf90a09eb15d8cc534f4e9de1332e510de42
destinl <1374214766@qq.com>
2024-07-23 23:15:21 +0800

    递归法 反转链表

--- demo1/src/main/java/linkedlist/ReverseLinkedlist.java
+++ demo1/src/main/java/linkedlist/ReverseLinkedlist.java
@@ -75,3 +75,28 @@ public class ReverseLinkedlist {
 
 
 }
+
+
+//递归法 反转链表
+//import java.util.Currency;
+//import java.util.List;
+//
+//class ListNode{
+//    int val;
+//    ListNode next;
+//    ListNode(int x ){val = x;}
+//}
+//public class Solution {
+//    public ListNode reverseList(ListNode head){
+//        if(head == null || head.next == null){
+//            return head;
+//        }
+//
+//        ListNode newHead = reverseList(head.next);
+//        head.next.next = head;
+//        head.next = null;
+//        return newHead;
+//    }
+//
+//
+//}
