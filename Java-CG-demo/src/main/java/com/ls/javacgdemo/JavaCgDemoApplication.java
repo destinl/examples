6350b151c9033888d9c896b512b61121642fa930
destinl <1374214766@qq.com>
2024-07-24 21:25:34 +0800

    idea插件javacg生成代码实践（没有显示生成的代码）

---
+++ Java-CG-demo/src/main/java/com/ls/javacgdemo/JavaCgDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ls.javacgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCgDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCgDemoApplication.class, args);
+	}
+
+}
---
+++ Java-CG-demo/src/test/java/com/ls/javacgdemo/JavaCgDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.javacgdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCgDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
