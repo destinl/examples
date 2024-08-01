f3d9e0ef16d3c17a43dcd11fdc6fe7752430ab39
destinl <1374214766@qq.com>
2024-08-01 23:05:00 +0800

    sentinel实践

---
+++ Sentinel-demo/src/main/java/com/ls/sentinel_demo/common/TaskResourceType.java
@@ -0,0 +1,14 @@
+package com.ls.sentinel_demo.common;
+
+/**
+ * @Description:
+ * @Author: ls
+ * @Date: 2024/8/1 21:40
+ */
+public class TaskResourceType {
+
+    public static String task1 = "task1";
+
+    public static String task2 = "task2";
+
+}
