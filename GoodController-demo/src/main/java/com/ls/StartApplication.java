331903c79ace8c3edd65113ff9a42c10dbf6c5a5
destinl <1374214766@qq.com>
2024-08-08 22:48:08 +0800

    nacos简单实践（看官方文档demo）

--- GoodController-demo/src/main/java/com/ls/StartApplication.java
+++ GoodController-demo/src/main/java/com/ls/StartApplication.java
@@ -1,16 +1,21 @@
 package com.ls;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * @Description:
+ * @Description: 记得启动nacos服务 startup.cmd -m standalone
  * @Author: ls
  * @Date: 2024/8/2 21:02
  */
 @SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableConfigServer
+//@NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class StartApplication {
 
     public static void main(String[] args) {
---
+++ GoodController-demo/src/main/java/com/ls/controller/ConfigController.java
@@ -0,0 +1,27 @@
+package com.ls.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @Description:
+ * @Author: ls
+ * @Date: 2024/8/8 21:01
+ */
+@Controller
+@RequestMapping("config")
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
+}
