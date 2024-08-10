b2d6f23f4031cef75f8d2ed6060892a4630440e8
destinl <1374214766@qq.com>
2024-08-10 15:09:33 +0800

    什么是 CSRF？如何防止 CSRF 攻击？（使用@EnableWebSecurity和@Configuration这两个注解会报错error页面404）

--- SSM_EXP/src/main/java/com/ls/ssm_exp/Application.java
+++ SSM_EXP/src/main/java/com/ls/ssm_exp/Application.java
@@ -13,6 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args); // 启动Spring Boot应用
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- SSM_EXP/src/main/java/com/ls/ssm_exp/config/WebSecurityConfig.java
+++ SSM_EXP/src/main/java/com/ls/ssm_exp/config/WebSecurityConfig.java
@@ -2,8 +2,10 @@ package com.ls.ssm_exp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().and()
             .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
@@ -34,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .logout()
                 .permitAll();
+//        http.csrf().and().authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
     }
 
 //    @Autowired
