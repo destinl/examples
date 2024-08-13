5f0d49f7c30e6be294d3afeb3da27dff6575450a
destinl <1374214766@qq.com>
2024-08-13 21:37:51 +0800

    敏感词过滤工具 sensitive-word 详解

---
+++ demo1/src/main/java/interesting/CustomSensitiveWordStrategy.java
@@ -0,0 +1,23 @@
+package interesting;
+
+
+//import com.github.houbb.sensitive.word.api.ISensitiveWord;
+//import com.github.houbb.sensitive.word.support.replace.SensitiveWordReplaceChar;
+
+/**
+ * @Description: 自定义检测和替换策略
+ * @Author: ls
+ * @Date: 2024/8/13 21:30
+ */
+
+public class CustomSensitiveWordStrategy {
+    public static void main(String[] args) {
+//        ISensitiveWord sensitiveWord = SensitiveWordBs.newInstance()
+//                .replace(SensitiveWordReplaceChar.newInstance())
+//                .init();
+//
+//        String text = "自定义敏感词检测和替换策略的示例。";
+//        String result = sensitiveWord.replace(text, '*');
+//        System.out.println("替换后的文本: " + result);
+    }
+}
---
+++ demo1/src/main/java/interesting/SensitiveWordExample.java
@@ -0,0 +1,31 @@
+package interesting;
+
+//import com.github.houbb.sensitive.word.api.ISensitiveWord;
+//import com.github.houbb.sensitive.word.bs.SensitiveWordBs;
+import java.util.List;
+/**
+ * @Description:
+ * @Author: ls
+ * @Date: 2024/8/13 21:19
+ */
+
+public class SensitiveWordExample {
+    public static void main(String[] args) {
+//        ISensitiveWord sensitiveWord = SensitiveWordBs.newInstance().init();
+//        String text = "这是一段包含敏感词的文本，比如一些非法的内容。";
+//
+//        // 查找敏感词
+//        List<String> wordList = sensitiveWord.findAll(text);
+//        System.out.println("敏感词列表: " + wordList);
+//
+//        // 替换敏感词
+//        String result = sensitiveWord.replace(text, '*');
+//        System.out.println("替换后的文本: " + result);
+//
+//        String text1 = "请访问我们的官方网站：https://example.com 或发送邮件至 support@example.com。";
+//
+//        // 检测并替换
+//        String result = sensitiveWord.replace(text1, '*');
+//        System.out.println("替换后的文本: " + result);
+    }
+}
