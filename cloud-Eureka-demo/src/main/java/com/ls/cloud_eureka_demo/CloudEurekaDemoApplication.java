6b7a0f23af2cc129200d7bdb2822310caf103675
destinl <1374214766@qq.com>
2024-09-20 23:44:46 +0800

    feat: cloud-Eureka-demo模块：注册中心Eureka简单认识实践（未完）

---
+++ cloud-Eureka-demo/src/main/java/com/ls/cloud_eureka_demo/CloudEurekaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ls.cloud_eureka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaDemoApplication.class, args);
+    }
+
+}
---
+++ cloud-Eureka-demo/src/test/java/com/ls/cloud_eureka_demo/CloudEurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.cloud_eureka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
